33d6e879b8739589f960d3915bfe254f30bbec44
KimDoHyeon <ehcjswo1@gmail.com>
2023-07-28 18:15:18 +0900

    07-28

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Trending from "app/(home)/Trending";
 import Tech from "app/(home)/Tech";
+import Travel from "app/(home)/Travel";
+import Other from "app/(shared)/Other";
+import Subscribe from "app/(shared)/Subscribe";
+import Sidebar from "app/(shared)/Sidebar";
 
 export default function Home() {
   return (
@@ -7,11 +11,15 @@ export default function Home() {
       <Trending />
       <div className="md:flex gap-10 mb-5">
         <Tech />
-        {/* <Travel /> */}
-        {/* <Other /> */}
-        <div className="hidden md:block">{/* <Subscribe /> */}</div>
+        <Travel />
+        <Other />
+        <div className="hidden md:block">
+          <Subscribe />
+        </div>
       </div>
-      <div className="basic-1/4">{/* <Sidebar /> */}</div>
+      <div className="basic-1/4">
+        <Sidebar />
+      </div>
     </main>
   );
 }
