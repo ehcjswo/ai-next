fc9735901b0de8280a6c5415d73d8d3cb210cb96
KimDoHyeon <ehcjswo1@gmail.com>
2023-08-01 15:17:16 +0900

    08-01

--- app/(home)/Tech.tsx
+++ app/(home)/Tech.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Card from "app/(shared)/Card";
+import { Post } from "@prisma/client";
 
-type Props = {};
+type Props = {
+  techPosts: Array<Post>;
+};
 
-const Tech = (props: Props) => {
+const Tech = ({ techPosts }: Props) => {
   return (
     <section>
       <hr className="border-1" />
@@ -29,22 +32,26 @@ const Tech = (props: Props) => {
         <Card
           className="bg-wh-500 col-span-1 row-span-3"
           imageHeight="h-96"
+          post={techPosts[0]}
           isLongForm
         ></Card>
         {/* SMALL CARD */}
         <Card
           className="bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3"
           imageHeight="h-48"
+          post={techPosts[1]}
           isSmallCard
         ></Card>
         <Card
           className="bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3"
           imageHeight="h-48"
+          post={techPosts[2]}
           isSmallCard
         ></Card>
         <Card
           className="bg-wh-500 col-span-1 row-span-1 mt-10 sm:mt-0 flex justify-between gap-3"
           imageHeight="h-48"
+          post={techPosts[3]}
           isSmallCard
         ></Card>
       </div>
--- app/(shared)/Card.tsx
+++ app/(shared)/Card.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import { Post } from "@prisma/client";
 
 type Props = {
   className?: string;
+  post: Post;
   imageHeight: string;
   isSmallCard?: boolean;
   isLongForm?: boolean;
@@ -11,28 +13,37 @@ type Props = {
 const Card = ({
   className,
   imageHeight,
+  post,
   isSmallCard = false,
   isLongForm = false,
 }: Props) => {
+  const { id, title, author, createdAt, image, snippet } = post || {};
+  const date = new Date(createdAt);
+  const options = { year: "numeric", month: "long", day: "numeric" } as any;
+  const formattedDate = date.toLocaleDateString("ko-KR", options);
+
   return (
     <div className={className}>
-      <Link className="basis-full hover:opacity-70" href="/">
+      <Link
+        className="basis-full hover:opacity-70"
+        href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+      >
         <div className={`relative w-auto mb-3 ${imageHeight}`}>image</div>
       </Link>
       <div className="basis-full">
-        <Link href="/">
+        <Link href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}>
           <h4
             className={`font-bold hover:text-accent-green
             ${isSmallCard ? "text-base" : "text-lg"}
             ${isSmallCard ? "line-clamp-2" : ""}
           `}
           >
-            title
+            {title}
           </h4>
         </Link>
         <div className={`${isSmallCard ? "my-2" : "flex my-3"} gap-3`}>
-          <h5 className="font_semibold text-xs">author</h5>
-          <h6 className="text-wh-300 text-xs">date</h6>
+          <h5 className="font_semibold text-xs">{author}</h5>
+          <h6 className="text-wh-300 text-xs">{formattedDate}</h6>
         </div>
         <p
           className={`text-wh-100 ${
